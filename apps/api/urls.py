c9eb3139b41ff2b21c780a02dc4f9ca9db93d97c
LucasGeneroso <lucasgeneroso189@gmail.com>
2023-05-20 13:48:08 -0300

    feat: Add restframework api

---
+++ apps/api/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import (
+    HabilidadeViewSet, 
+    PedidoViewSet, 
+    PerfilViewSet, 
+    ServicoViewSet, 
+    AvaliacaoViewSet, 
+    MensagemViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register('habilidades', HabilidadeViewSet)
+router.register('pedidos', PedidoViewSet)
+router.register('perfis', PerfilViewSet)
+router.register('servicos', ServicoViewSet)
+router.register('avaliacoes', AvaliacaoViewSet)
+router.register('mensagens', MensagemViewSet)
+
+api_router = [
+    # outras urls do seu projeto
+    path('api/', include(router.urls)),
+]
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Habilidade, Perfil, Servico, Avaliacao, Pedido, Mensagem
+from .serializers import (
+    HabilidadeSerializer, 
+    PedidoSerializer, 
+    PerfilSerializer, 
+    ServicoSerializer, 
+    AvaliacaoSerializer, 
+    MensagemSerializer,
+)
 
-# Create your views here.
+
+# /habilidades > route url > viewset > get data in model > return with serializer
+class HabilidadeViewSet(viewsets.ModelViewSet):
+    queryset = Habilidade.objects.all()
+    serializer_class = HabilidadeSerializer
+
+
+class PedidoViewSet(viewsets.ModelViewSet):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
+
+
+class PerfilViewSet(viewsets.ModelViewSet):
+    queryset = Perfil.objects.all()
+    serializer_class = PerfilSerializer
+
+
+class ServicoViewSet(viewsets.ModelViewSet):
+    queryset = Servico.objects.all()
+    serializer_class = ServicoSerializer
+
+
+class AvaliacaoViewSet(viewsets.ModelViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+
+class MensagemViewSet(viewsets.ModelViewSet):
+    queryset = Mensagem.objects.all()
+    serializer_class = MensagemSerializer
