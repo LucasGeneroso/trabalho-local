04a67afd3d316d19887488ed7e083d313ac13958
Lucas Generoso <lucasgeneroso189@gmail.com>
2023-05-27 10:27:30 -0300

    fix: API session authentication (#20)
    Fix API Session Authentication.

--- trabalho_local/settings.py
+++ trabalho_local/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
